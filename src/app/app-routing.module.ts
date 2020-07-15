448d411ffb42add028b3c9a3894debe94d7091c1
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-07-15 17:31:43 +0200

    adding clients module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const appRoutes: Routes = [
   { path: '', redirectTo: '/restaurants', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)},
   { path: 'restaurants', loadChildren: () => import('./restaurants/restaurants.module').then(m => m.RestaurantsModule)},
+  { path: 'clients', loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule)},
   { path: 'error', component: PageNotFoundComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
---
+++ src/app/clients/clients-routing.module.ts
@@ -0,0 +1,15 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {ClientsComponent} from './clients.component';
+
+const routes: Routes = [
+  {path: '',
+    component: ClientsComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ClientsRoutingModule { }
