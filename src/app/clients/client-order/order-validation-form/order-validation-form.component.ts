b5ae021bd88a1ba68bc0dd57e334011a53042a24
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-07-29 14:40:24 +0200

    initial commit for validation order

---
+++ src/app/clients/client-order/order-validation-form/order-validation-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-validation-form',
+  templateUrl: './order-validation-form.component.html',
+  styleUrls: ['./order-validation-form.component.css']
+})
+export class OrderValidationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
